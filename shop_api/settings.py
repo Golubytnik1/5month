4d665edbe5d1a1df022a17c920c177c580a6aefa
Golubytnik1 <foxycool2005@gmail.com>
2023-04-13 01:17:25 +0600

    hw7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--paio=oi&q(x)r4p$*0x%4zx)!bf+33yac3$vr(xmtg*jtpu20'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_db',
+        'USER': 'tester',
+        'PASSWORD': '123456',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
 
